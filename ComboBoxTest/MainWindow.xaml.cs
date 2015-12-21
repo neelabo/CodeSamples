b2188e299a086387863b7baec1ba8fef64e652f3
nee <nee.laboratory@gmail.com>
2015-12-21 20:01:26 +0900

    combobox enum sample

---
+++ ComboBoxTest/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ComboBoxTest
+{
+    public enum Animal
+    {
+        Cat,
+        Dog,
+        Tiger,
+        Elephant,
+    }
+
+    public partial class MainWindow : Window
+    {
+        public Animal[] AnimalEnums { get; } = (Animal[])Enum.GetValues(typeof(Animal));
+
+        public Dictionary<Animal, string> AnimalNames { get; } = new Dictionary<Animal, string>()
+        {
+            [Animal.Cat] = "猫",
+            [Animal.Dog] = "犬",
+            [Animal.Tiger] = "虎",
+            [Animal.Elephant] = "ゾウ",
+        };
+
+        public Animal MyPet { get; set; } = Animal.Cat;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+    }
+}
